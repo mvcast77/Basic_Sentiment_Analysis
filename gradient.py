1569883d8f934ecee6a1aa4939bb926174befd4f
mvcast77 <castillo2020var@gmail.com>
2025-04-17 21:18:00 -0700

    [ADD/EDIT] Change Sigmoid, and generate prediction
    Changing the sigmoid to rely on the fact that np.exp will operate
    elementwise, this actually simplifies the function a great deal.
    
    Creating outputter function to write prediction to a file of choice.
    This is done prediction by prediction to deal with the fact that
    ypredictions is an array of 1-element arrays.

--- gradient.py
+++ gradient.py
@@ -2,21 +2,35 @@ import numpy as np
 import math
 from utils import *
 
-alpha = 10e-4
+alpha = 1e-4
+epsilon = 1e-8
 xtrain = "x_train.csv"
 ytrain = "y_train.csv"
+xtest = "x_test.csv"
+ypred = "y_pred.csv"
 
 def S(array):
-    ret_array = np.ones_like(array)
-    for spot, element in zip(array, ret_array):
-        spot = 1 / (1 + math.exp(-element))
-    return ret_array
+    ret_array = np.clip(array, -500, 500)
+    return 1 / (1 + np.exp(-ret_array))
 
+def magnitude(vec):
+    sum = 0.0
+    for x in vec:
+        sum += (x * x)
+    return math.sqrt(sum)
 
-X, y, w = initialize(xtrain, ytrain)
+X, y, w, X_test = initialize(xtrain, ytrain, xtest)
 
 X_T = np.transpose(X)
 
-# while("""condition"""):
-#     w = w_update
-#     w_update = w + alpha * X_transposed * (y_result - S(matrix * w))
+w_update = w + alpha * np.dot(X_T, y - S(np.dot(X,w)))
+
+while(magnitude(w_update - w) > epsilon):
+    w = w_update
+    w_update = w + alpha * np.dot(X_T, y - S(np.dot(X,w)))
+    print(magnitude(w_update - w))
+w = w_update
+
+ypredictions = S(np.dot(X_test,w))
+
+outputter(ypredictions, ypred)
--- utils.py
+++ utils.py
@@ -1,26 +1,50 @@
+import sys
 from sklearn.feature_extraction.text import TfidfVectorizer
 import numpy as np
+np.set_printoptions(threshold=sys.maxsize)
 import pandas as pd
 
 
-def initialize(xtrain, ytrain):
+def initialize(xtrain, ytrain, xtest):
 
     data_frame = pd.read_csv(xtrain)
+    data_frame_test = pd.read_csv(xtest)
     y_result_df = pd.read_csv(ytrain)
     y_result = y_result_df.to_numpy()
 
     corpus = data_frame[['text']]
+    corpus_test = data_frame_test[['text']]
     corpus_list = [x for str in corpus['text'] if (x := str)]
+    N = len(corpus_list)
+    corpus_list_test = [x for str in corpus_test['text'] if (x := str)]
+    N_test = len(corpus_list_test)
+    corpus_list = corpus_list + corpus_list_test
+    print(N)
+    print(N_test)
+    print(N + N_test)
+    print(len(corpus_list))
 
     vectorizer = TfidfVectorizer()
     matrix = vectorizer.fit_transform(corpus_list)
     matrix = matrix.toarray()
 
-    N = matrix.shape[0]
+    matrix, matrix_test = np.split(matrix, [N]);
     p = matrix.shape[1]
 
     matrix = np.c_[matrix, np.ones(N)]
+    matrix_test = np.c_[matrix_test, np.ones(N_test)]
 
-    weights = np.ones((p+1, 1))
+    weights = np.full((p+1, 1), 0.5)
 
-    return matrix, y_result, weights
+    print(matrix.shape)
+    print(matrix_test.shape)
+    print(y_result.shape)
+    print(weights.shape)
+
+    return matrix, y_result, weights, matrix_test
+
+def outputter(ypredictions, y_file):
+    fobj = open(y_file, 'w')
+    for predictions in ypredictions:
+        fobj.write(str(predictions[0]) + "\n")
+    fobj.close()
